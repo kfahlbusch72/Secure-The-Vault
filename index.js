e56efc8863a01b95b59c2065ab9b582db5db7017
karl <karl.fahlbusch7241@gmail.com>
2025-06-11 12:36:22 -0500

    ain

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("Secure The Vault");
+
+let code1 = 10;
+
+// code1 = "7 + 3";
+
+console.log(code1);
+
+let code2 = 40;
+
+// code2 = "120 / 2 - 20";
+
+console.log(code2);
+
+let code3 = 39;
+
+// code3 = "80 % 41";
+
+console.log(code3);
+
+let message = "the vault had been secured. the combinations is:";
+
+console.log(message);
+
+let codeA = "10-40-39";
+
+// codeA = "code1 + code2 + code3";
+
+console.log(codeA);
+
+let codeB = "10-40-39";
+
+// codeB = "&{code1} + &{code2} + &{code3}";
+
+console.log(codeB);
